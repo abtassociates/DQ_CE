42ea1d766890db80c56dbaa7dd32fa43183bf4fb
Samantha Kameyama <kameyamas@aneprr-wxo.ad.abt.local>
2023-05-22 10:33:45 -0400

    loading sample data for inventory

---
+++ sandbox/sample-data/sample-data.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+library(lubridate)
+
+# Instructions ------------------------------------------------------------
+
+# Run this, it will create a data directory here in case you don't already have one
+
+if(dir.exists("sandbox/mini-non-shiny-environment/data/")) {
+  "All good"
+} else {
+  dir.create("sandbox/mini-non-shiny-environment/data/")
+}
+
+directory <- "sandbox/mini-non-shiny-environment/"
+
+# Copy a sample dataset into the mini-non-shiny-environment/data directory
+
+if_else(file.exists(paste0(directory, "data/Enrollment.csv")),
+        "All good",
+        "Please unzip a sample data set into your mini-non-shiny-environment/data
+        directory before proceeding.")
+
+# Run the scratch files in order. As you're running them, it's helpful to have
+# the Environment tab open so you can see what is going on
+
+# Functions ---------------------------------------------------------------
+source(here("global.R"))
+source(here("guidance.R"))
+
+source(paste0(directory, "helper_functions.R"))
+
+# Hard codes ---------------------------------------------------------------
+
+source(paste0(directory, "hardcodes.R"))
+
+# Get Export --------------------------------------------------------------
+
+source(paste0(directory, "01_get_Export.R"))
+
+# Export Dates -------------------------------------------------------------------
+
+source(paste0(directory, "02_export_dates.R"))
+
+# Date Range Check --------------------------------------------------------
+
+diff.Date()
