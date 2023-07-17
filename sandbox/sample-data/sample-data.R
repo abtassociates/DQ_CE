8ee295d70eb7ece4f4f1e02733b96800cc19f2b9
Samantha Kameyama <kameyamas@aneprr-xip.ad.abt.local>
2023-07-17 09:11:35 -0400

    Stayers and fixed multiple enrollments

--- sandbox/sample-data/sample-data.R
+++ sandbox/sample-data/sample-data.R
@@ -66,15 +66,10 @@ ES_enrollments <- Enrollment %>%
   distinct(EnrollmentID) %>%
   n_distinct()
 
-ES_enrollments <- Enrollment %>%
+ES_stayers <- Enrollment %>%
   group_by(ProjectType) %>%
-  filter(ProjectType == "1") %>%
-  distinct(EnrollmentID) %>%
-  n_distinct()
-
-ES_enrollments <- Enrollment %>%
-  group_by(ProjectType) %>%
-  filter(ProjectType == "1") %>%
+  filter(is.na(ExitDate) &
+    ProjectType == "1") %>%
   distinct(EnrollmentID) %>%
   n_distinct()
 
@@ -84,15 +79,36 @@ TH_enrollments <- Enrollment %>%
   distinct(EnrollmentID) %>%
   n_distinct()
 
+TH_stayers <- Enrollment %>%
+  group_by(ProjectType) %>%
+  filter(is.na(ExitDate) &
+    ProjectType == "2") %>%
+  distinct(EnrollmentID) %>%
+  n_distinct()
+
 PSH_enrollments <- Enrollment %>%
   group_by(ProjectType) %>%
   filter(ProjectType == "3") %>%
   distinct(EnrollmentID) %>%
   n_distinct()
 
+PSH_stayers <- Enrollment %>%
+  group_by(ProjectType) %>%
+  filter(is.na(ExitDate) &
+    ProjectType == "3") %>%
+  distinct(EnrollmentID) %>%
+  n_distinct()
+
 SO_enrollments <- Enrollment %>%
   group_by(ProjectType) %>%
   filter(ProjectType == "4") %>%
+  distinct(EnrollmentID) %>%
+  n_distinct()
+
+SO_stayers <- Enrollment %>%
+  group_by(ProjectType) %>%
+  filter(is.na(ExitDate) &
+    ProjectType == "4") %>%
   distinct(EnrollmentID) %>%
   n_distinct()
   
@@ -102,9 +118,23 @@ DS_enrollments <- Enrollment %>%
   distinct(EnrollmentID) %>%
   n_distinct()
 
+DS_stayers <- Enrollment %>%
+  group_by(ProjectType) %>%
+  filter(is.na(ExitDate) &
+    ProjectType == "11") %>%
+  distinct(EnrollmentID) %>%
+  n_distinct()
+
 PH_Svcs_enrollments <- Enrollment %>%
   group_by(ProjectType) %>%
   filter(ProjectType == "10") %>%
+  distinct(EnrollmentID) %>%
+  n_distinct()
+
+PH_Svcs_stayers <- Enrollment %>%
+  group_by(ProjectType) %>%
+  filter(is.na(ExitDate) &
+    ProjectType == "10") %>%
   distinct(EnrollmentID) %>%
   n_distinct()
 
@@ -114,9 +144,23 @@ PH_NoSvcs_enrollments <- Enrollment %>%
   distinct(EnrollmentID) %>%
   n_distinct()
 
+PH_NoSvcs_stayers <- Enrollment %>%
+  group_by(ProjectType) %>%
+  filter(is.na(ExitDate) &
+    ProjectType == "9") %>%
+  distinct(EnrollmentID) %>%
+  n_distinct()
+
 SSO_enrollments <- Enrollment %>%
   group_by(ProjectType) %>%
   filter(ProjectType == "6") %>%
+  distinct(EnrollmentID) %>%
+  n_distinct()
+
+SSO_stayers <- Enrollment %>%
+  group_by(ProjectType) %>%
+  filter(is.na(ExitDate) &
+    ProjectType == "6") %>%
   distinct(EnrollmentID) %>%
   n_distinct()
 
@@ -126,9 +170,23 @@ HP_enrollments <- Enrollment %>%
   distinct(EnrollmentID) %>%
   n_distinct()
 
+HP_stayers <- Enrollment %>%
+  group_by(ProjectType) %>%
+  filter(is.na(ExitDate) &
+    ProjectType == "12") %>%
+  distinct(EnrollmentID) %>%
+  n_distinct()
+
 RRH_enrollments <- Enrollment %>%
   group_by(ProjectType) %>%
   filter(ProjectType == "13") %>%
+  distinct(EnrollmentID) %>%
+  n_distinct()
+
+RRH_stayers <- Enrollment %>%
+  group_by(ProjectType) %>%
+  filter(is.na(ExitDate) &
+    ProjectType == "13") %>%
   distinct(EnrollmentID) %>%
   n_distinct()
 
@@ -138,48 +196,78 @@ CE_enrollments <- Enrollment %>%
   distinct(EnrollmentID) %>%
   n_distinct()
 
+CE_stayers <- Enrollment %>%
+  group_by(ProjectType) %>%
+  filter(is.na(ExitDate) &
+    ProjectType == "14") %>%
+  distinct(EnrollmentID) %>%
+  n_distinct()
+
 services <- Services %>%
   distinct(ServicesID) %>%
   n_distinct()
+print(services)
 
 mult_enrollments <- Enrollment %>%
   group_by(PersonalID) %>%
   summarise(NumEnrollmentID = n()) %>%
   filter(NumEnrollmentID > 1) %>%
-  summarise(NumPersonalID = n())
-print(mult_enrollments)
+  summarise(NumPersonalID = n()) %>%
+  pull(NumPersonalID)
 
 sample_data <- data.frame(
   Data = c("Number of projects", 
            "Number of organizations", 
            "Number of enrollments",
            "Number of ES enrollments",
+           "Number of ES stayers",
            "Number of TH enrollments",
+           "Number of TH stayers",
            "Number of PSH enrollments",
+           "Number of PSH stayers",
            "Number of SO enrollments",
+           "Number of SO stayers",
            "Number of DS enrollments",
-           "Number of PH with Services enrollments",
-           "Number of PH No Services enrollments",
+           "Number of DS stayers",
+           "Number of PH with Svcs enrollments",
+           "Number of PH with Svcs stayers",
+           "Number of PH No Svcs enrollments",
+           "Number of PH No Svcs stayers",
            "Number of SSO enrollments",
+           "Number of SSO stayers",
            "Number of HP enrollments",
+           "Number of HP stayers",
            "Number of RRH enrollments",
+           "Number of RRH stayers",
            "Number of CE enrollments",
+           "Number of CE stayers",
            "Number of services",
            "Number of clients with multiple enrollments"),
   Number = c(projects, 
              organizations, 
              projects,
              ES_enrollments,
+             ES_stayers,
              TH_enrollments,
+             TH_stayers,
              PSH_enrollments,
+             PSH_stayers,
              SO_enrollments,
+             SO_stayers,
              DS_enrollments,
+             DS_stayers,
              PH_Svcs_enrollments,
+             PH_Svcs_stayers,
              PH_NoSvcs_enrollments,
+             PH_NoSvcs_stayers,
              SSO_enrollments,
+             SSO_stayers,
              HP_enrollments,
+             HP_stayers,
              RRH_enrollments,
+             RRH_stayers,
              CE_enrollments,
+             CE_stayers,
              services,
              mult_enrollments))
 print(sample_data)
